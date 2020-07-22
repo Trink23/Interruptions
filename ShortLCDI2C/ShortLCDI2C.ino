ad56fe8ca1a7270894cc64c994898add6ddb9b13
Trink23 <34166714+Trink23@users.noreply.github.com>
2020-07-22 07:14:31 +0100

    Updated AC test cases completed
    All AC cases added on progress, I will work on  the DC cases today evening 07/22/20

--- ShortLCDI2C/ShortLCDI2C.ino
+++ ShortLCDI2C/ShortLCDI2C.ino
@@ -58,8 +58,6 @@ void setup() {
 }
 
 void loop() {
-  // Print 'Scale' on the first line of the LCD:   
-  Serial.println(digitalRead(enter));
   delay(100);
 
   if(digitalRead(left) == 0){
@@ -69,22 +67,26 @@ void loop() {
 }
 
 void ACMenu(){
+  ACInputVoltageTestCases(noCase); // Updates the lcd and change the TimeInterruption 
   while(1){
-    delay(100);
+    delay(300);
     if(digitalRead(down) == 0){
-      if(noCase >= 1){
+      if(noCase > 1){
         noCase--;
+        Serial.println("noCase " + String(noCase)); 
         ACInputVoltageTestCases(noCase); // Updates the lcd and change the TimeInterruption
       }
     }
     else if(digitalRead(up) == 0){
-      if(noCase <= 6){      
-        noCase++;   
+      if(noCase < 6){      
+        noCase++; 
+        Serial.println("noCase " + String(noCase));   
         ACInputVoltageTestCases(noCase); // Updates the lcd and change the TimeInterruption  
       }     
     }
     else if(digitalRead(enter) == 0){    
       executeInterruption(interruption); // Execute the Interruption 
+      Serial.println("noCase " + String(noCase)); 
       ACInputVoltageTestCases(noCase); // Updates the lcd and change the TimeInterruption       
     }
        
@@ -94,21 +96,8 @@ void ACMenu(){
 
 void executeInterruption(int interruptionTime){
 
-  lcd.clear();
-  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
-  lcd.print("TEST IN PROGRESS");
-  lcd.setCursor(0, 1); // Set the cursor on the first column and first row.
-  lcd.print("     5");
-  delay(1000);
-  lcd.print("     4");
-  delay(1000);
-  lcd.print("     3");
-  delay(1000);
-  lcd.print("     2");
-  delay(1000);
-  lcd.print("     1");
-  delay(1000);
-  
+  Serial.println("Check miliseconds selected " + String(interruptionTime));      
+  coundown();
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
   delayMicroseconds(5);
   digitalWrite(SecondGroupRelays, LOW); // later
@@ -116,10 +105,43 @@ void executeInterruption(int interruptionTime){
   digitalWrite(SecondGroupRelays, HIGH); // later
   delayMicroseconds(5);
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  ACInputVoltageTestCases(noCase); // Updates the lcd and change the TimeInterruption
                          
   
 }
-
+void coundown(){
+  lcd.clear();
+  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
+  lcd.print("TEST IN PROGRESS");
+  lcd.setCursor(0, 1); // Set the cursor on the first column and first row.
+  lcd.print("     5");  
+  delay(1000);
+  lcd.clear();
+  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
+  lcd.print("TEST IN PROGRESS");
+  lcd.setCursor(0, 1); // Set the cursor on the first column and first row.
+  lcd.print("     4");
+  delay(1000);
+  lcd.clear();
+  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
+  lcd.print("TEST IN PROGRESS");
+  lcd.setCursor(0, 1); // Set the cursor on the first column and first row.
+  lcd.print("     3");
+  delay(1000);
+  lcd.clear();
+  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
+  lcd.print("TEST IN PROGRESS");
+  lcd.setCursor(0, 1); // Set the cursor on the first column and first row.
+  lcd.print("     2");
+  delay(1000);
+  lcd.clear();
+  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
+  lcd.print("TEST IN PROGRESS");
+  lcd.setCursor(0, 1); // Set the cursor on the first column and first row.
+  lcd.print("     1");
+  delay(1000);
+  
+}
 void ACInputVoltageTestCases(int caseValue){
  switch(caseValue){
     case 1:
@@ -129,6 +151,7 @@ void ACInputVoltageTestCases(int caseValue){
       lcd.setCursor(0, 1); // Set the cursor on the first column and first row.
       lcd.print("V2: 0%  10ms"); // Print string 
       interruption = 10;  
+      break;
     case 2:
       lcd.clear(); // Clear the screen
       lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
@@ -136,6 +159,7 @@ void ACInputVoltageTestCases(int caseValue){
       lcd.setCursor(0, 1); // Set the cursor on the first column and first row.
       lcd.print("V2: 0%  20ms"); // Print string 
       interruption = 20;  
+      break;
     case 3:
       lcd.clear(); // Clear the screen
       lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
@@ -143,6 +167,7 @@ void ACInputVoltageTestCases(int caseValue){
       lcd.setCursor(0, 1); // Set the cursor on the first column and first row.
       lcd.print("V2: 40%  200ms"); // Print string 
       interruption = 200;
+      break;
     case 4:
       lcd.clear(); // Clear the screen
       lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
@@ -150,6 +175,7 @@ void ACInputVoltageTestCases(int caseValue){
       lcd.setCursor(0, 1); // Set the cursor on the first column and first row.
       lcd.print("V2: 70%  500ms"); // Print string 
       interruption = 500;
+      break;
     case 5:
       lcd.clear(); // Clear the screen
       lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
@@ -157,6 +183,7 @@ void ACInputVoltageTestCases(int caseValue){
       lcd.setCursor(0, 1); // Set the cursor on the first column and first row.
       lcd.print("V2: 80%  5000ms"); // Print string 
       interruption = 5000;
+      break;
     case 6:
       lcd.clear(); // Clear the screen
       lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
@@ -164,7 +191,7 @@ void ACInputVoltageTestCases(int caseValue){
       lcd.setCursor(0, 1); // Set the cursor on the first column and first row.
       lcd.print("V2: 0%  5000ms"); // Print string 
       interruption = 5000;
-    default:
-      lcd.print("Hola Mundo");     
+      break;
+ 
   }
 }
